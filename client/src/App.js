2de871c59d3799e1d2e73f7a94f77269f8085b40
mrlopez116 <mariolopez@Marios-MacBook-Pro-2.local>
2019-05-27 11:43:49 +0200

    Database connected to React frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ import "./App.css";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = {
+             apiResponse: "",
+             dbResponse: ""
+        };
     }
 
     callAPI() {
@@ -15,8 +18,17 @@ class App extends Component {
             .catch(err => err);
     }
 
+    callDB() {    
+        fetch("http://localhost:9000/testDB")        
+            .then(res => res.text())        
+            .then(res => this.setState({ dbResponse: res }))        
+            .catch(err => err);
+    }
+
+
     componentDidMount() {
         this.callAPI();
+        this.callDB();
     }
 
     render() {
@@ -27,6 +39,7 @@ class App extends Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">{this.state.apiResponse}</p>
+                <p className="App-intro">{this.state.dbResponse}</p>
             </div>
         );
     }
